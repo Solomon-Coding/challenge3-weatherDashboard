0d140c1b67b83085110c7293c3a9018bc7e6a241
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-01-05 19:03:24 -0600

    Added APIs to js script.. still need to extract variables from the geological API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-var APIKey = "1b97f3650e5d31f18a2fae6982fb7779";
+var APIKey = "72ddd287ed4d6c4333abbfd25983a46c";
+var lat=44;
+var lon=32;
+var city = "London"
+var locationURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
+var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+
+fetch(locationURL,
+    {
+      cache: "reload"
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        console.log(data.lat)
+        console.log(data.lon)
+      });
+
+
+
+fetch(weatherURL,
+{
+  cache: "reload"
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
