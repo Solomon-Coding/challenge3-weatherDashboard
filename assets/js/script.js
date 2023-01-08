150086988b9a5c0abe7c7b26c1ea8983abfd49e1
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-01-08 15:13:54 -0600

    Have both API calls they both dont work in series

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,25 @@
 var APIKey = "72ddd287ed4d6c4333abbfd25983a46c";
-var lat=44;
-var lon=32;
+
 var city = "London"
 var locationURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
-var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
 fetch(locationURL,
     {
-      cache: "reload"
+        cache: "reload"
     })
-      .then(function (response) {
+        .then(function (response) {
         return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-        console.log(data.lat)
-        console.log(data.lon)
-      });
+        })
+        .then(function (data) {
+        var lat=data[0].lat;
+        var lon=data[0].lon;
+        console.log(lat)
+        console.log(lon)
+        weather(lat,lon)
+        });
 
-
+console.log(lon)
+      var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
 fetch(weatherURL,
 {
@@ -30,3 +31,17 @@ fetch(weatherURL,
   .then(function (data) {
     console.log(data);
   });
+
+
+// //   starting files:
+//   touch index.html
+//   mkdir assets 
+//   cd assets/ 
+//   mkdir css 
+//   mkdir js 
+//   mkdir images 
+//   cd css/ 
+//   touch style.css
+//   cd .. 
+//   cd js/
+//   touch script.js 
