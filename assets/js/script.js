02c8f0251a13b450791448e8f76bc280b18cd82e
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-01-08 18:52:32 -0600

    Added first card for the 5 day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var APIKey = "72ddd287ed4d6c4333abbfd25983a46c";
 var city = "London"
 var locationURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
 
-fetch(locationURL,
+function geoLocation(locationURL,APIKey){
+  fetch(locationURL,
     {
         cache: "reload"
     })
@@ -13,35 +14,26 @@ fetch(locationURL,
         .then(function (data) {
         var lat=data[0].lat;
         var lon=data[0].lon;
-        console.log(lat)
-        console.log(lon)
-        weather(lat,lon)
+        weather(lon,lat,APIKey)
         });
+}
 
-console.log(lon)
-      var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+function weather(lon,lat,APIKey){
+  var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
-fetch(weatherURL,
-{
-  cache: "reload"
-})
-  .then(function (response) {
-    return response.json();
+  fetch(weatherURL,
+  {
+    cache: "reload"
   })
-  .then(function (data) {
-    console.log(data);
-  });
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.list[0].main.temp)
+      
+    });
+}
 
 
-// //   starting files:
-//   touch index.html
-//   mkdir assets 
-//   cd assets/ 
-//   mkdir css 
-//   mkdir js 
-//   mkdir images 
-//   cd css/ 
-//   touch style.css
-//   cd .. 
-//   cd js/
-//   touch script.js 
+geoLocation(locationURL,APIKey);
