b079e7cb1fbc3bd3166f220970cdeb1de6f2dba8
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-01-08 22:06:18 -0600

    90% done with the five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 var APIKey = "72ddd287ed4d6c4333abbfd25983a46c";
-
 var city = "London"
 var locationURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey;
+var dateEl = document.querySelectorAll('.card-title');
+var tempEl = document.querySelectorAll('.temp');
+var windEl = document.querySelectorAll('.wind');
+var humidityEl = document.querySelectorAll('.humidity');
+var iconEl = document.querySelectorAll(".card-img-top");
 
 function geoLocation(locationURL,APIKey){
   fetch(locationURL,
@@ -30,13 +34,30 @@ function weather(lon,lat,APIKey){
     })
     .then(function (data) {
       console.log(data);
-      console.log(data.list[0].main.temp-273.15)
-      console.log(data.list[8].main.temp-273.15)
-      console.log(data.list[16].main.temp-273.15)
-      console.log(data.list[24].main.temp-273.15)
-      console.log(data.list[32].main.temp-273.15)
+      for (var i=1;i<dateEl.length;i++) {
+        dateEl[i].textContent=new Date(data.list[i*8].dt_txt).toLocaleDateString();
+        iconEl[i].src= "http://openweathermap.org/img/w/" + data.list[i*8].weather[0].icon + ".png";
+        tempEl[i].textContent=Math.round(data.list[i*8].main.temp-273.15) + "\u00B0" + "F";
+        windEl[i].textContent=data.list[i*8].wind.speed + " MPH";
+        humidityEl[i].textContent=data.list[i*8].main.humidity + "%";
+      }
     });
-}
+    var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
 
+    fetch(currentWeatherURL,
+      {
+        cache: "reload"
+      })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+            dateEl[0].textContent=new Date(data.dt).toLocaleDateString();
+            tempEl[0].textContent=Math.round(data.main.temp-273.15) + "\u00B0" + "F";
+            windEl[0].textContent=data.wind.speed + " MPH";
+            humidityEl[0].textContent=data.main.humidity + "%";
+            iconEl[0].src= "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        });
+}
 
 geoLocation(locationURL,APIKey);
